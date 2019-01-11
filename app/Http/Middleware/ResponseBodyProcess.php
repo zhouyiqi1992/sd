a47a81a28b154cf55b35f536d3f3cafa451acba3
周奕奇 <137968399@qq.com>
2019-01-11 19:13:28 +0800

    fixbug

--- app/Http/Middleware/ResponseBodyProcess.php
+++ app/Http/Middleware/ResponseBodyProcess.php
@@ -12,7 +12,12 @@ class ResponseBodyProcess
 
     public function handle($request, Closure $next)
     {
-        $response =  $next($request);
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        // $response->header('Access-Control-Allow-Credentials', 'true');
+
         $data = json_decode($response->getContent());
         if (!isset($data) || empty($data)) {
             return $response;
